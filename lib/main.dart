20a71dd3aaa0a237139bdf5622ea44653ea4aa16
enkudavi <ekopur05@gmail.com>
2021-12-22 11:23:52 +0700

    install google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,10 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/Union.png'),
-    );
+        body: Center(
+            child: Text(
+      'hello world',
+      style: GoogleFonts.poppins(fontSize: 30),
+    )));
   }
 }
