7953ba785a06d6b45c5976472a18637b36e1fa5a
enkudavi <ekopur05@gmail.com>
2021-12-28 15:24:30 +0700

    add new style color

--- lib/theme.dart
+++ lib/theme.dart
@@ -8,10 +8,12 @@ Color AlertColor = Color(0xffED6363);
 Color PriceColor = Color(0xff2C96F1);
 Color BackgroundColor1 = Color(0xff1F1D2B);
 Color BackgroundColor2 = Color(0xff2B2937);
+Color BackgroundColor3 = Color(0xff252836);
 Color BackgroundColor = Color(0xff242231);
 Color PrimaryTextColor = Color(0xffF1F0F2);
 Color SecondaryTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504F5E);
+Color BottomMenuColor = Color(0xff808191);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: PrimaryTextColor);
 TextStyle secondaryTextStyle = GoogleFonts.poppins(color: SecondaryTextColor);
